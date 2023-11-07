4e172909204b3f96c9fc44ccd5eb63c771206d50
Valentin Terebenin <terebenin.work@gmail.com>
2023-11-07 19:52:59 +0300

    feat: added rough merge sort

--- src/pages/sort.rs
+++ src/pages/sort.rs
@@ -1,7 +1,8 @@
 use rand::seq::SliceRandom;
 use rand::thread_rng;
+use web_sys::wasm_bindgen::{JsValue, intern};
 use yew::prelude::*;
-use gloo_timers::callback::Timeout;
+use gloo_timers::callback::{Timeout, Interval};
 
 const WIDTH: usize = 8;
 const CHART_WIDTH: i32 = 500;
@@ -13,43 +14,63 @@ fn get_translate(idx: usize) -> String {
     format!("translate({}, 0)", idx * (WIDTH + 2))
 }
 
-fn merge_sort<T: Ord + Clone>(arr: &mut Vec<T>) {
-    let len = arr.len();
-    if len <= 1 {
-        return;
+pub fn merge_sort<T: Copy + Clone + Ord>(items: &mut Vec<T>, steps: &mut Vec<(usize, T)>) {
+    _merge_sort(items, steps, 0);
+}
+
+fn _merge_sort<T: Copy + Clone + PartialOrd>(
+    items: &mut Vec<T>,
+    mut steps: &mut Vec<(usize, T)>,
+    start_i: usize,
+) {
+    if items.len() > 1 {
+        let middle = items.len() / 2;
+        let mut left_half = items[0..middle].to_vec();
+        let mut right_half = items[middle..].to_vec();
+        _merge_sort(&mut left_half, &mut steps, start_i);
+        _merge_sort(&mut right_half, &mut steps, start_i + middle);
+        *items = merge(left_half, right_half, &mut steps, start_i);
     }
+}
 
-    let mid = len / 2;
-    let mut left = arr[..mid].to_vec();
-    let mut right = arr[mid..].to_vec();
+fn merge<T: Copy + Clone + PartialOrd>(
+    a: Vec<T>,
+    b: Vec<T>,
+    steps: &mut Vec<(usize, T)>,
+    start_i: usize,
+) -> Vec<T> {
+    let size = a.len() + b.len();
+    let mut merged: Vec<T> = Vec::with_capacity(size);
 
-    merge_sort(&mut left);
-    merge_sort(&mut right);
+    let mut i = 0; // Idx for a
+    let mut j = 0; // Idx for b
 
-    let (mut i, mut j, mut k) = (0, 0, 0);
-
-    while i < left.len() && j < right.len() {
-        if left[i] <= right[j] {
-            arr[k] = left[i].clone();
+    // Loop through a and b, adding the smallest values between them to `merged`
+    while i < a.len() && j < b.len() {
+        if a[i] < b[j] {
+            merged.push(a[i]);
+            steps.push((start_i + merged.len() - 1, a[i]));
             i += 1;
         } else {
-            arr[k] = right[j].clone();
+            merged.push(b[j]);
+            steps.push((start_i + merged.len() - 1, b[j]));
             j += 1;
         }
-        k += 1;
     }
 
-    while i < left.len() {
-        arr[k] = left[i].clone();
+    // Add all remaining values
+    while i < a.len() {
+        merged.push(a[i]);
+        steps.push((start_i + merged.len() - 1, a[i]));
         i += 1;
-        k += 1;
+    }
+    while j < b.len() {
+        merged.push(b[j]);
+        steps.push((start_i + merged.len() - 1, b[j]));
+        j += 1;
     }
 
-    while j < right.len() {
-        arr[k] = right[j].clone();
-        j += 1;
-        k += 1;
-    }
+    merged
 }
 
 #[function_component(Sort)]
@@ -80,14 +101,30 @@ pub fn sort() -> Html {
         .collect::<Html>();
 
     let view_box = format!("0 0 {} {}", CHART_WIDTH, CHART_HEIGHT);
+    let time = 10;
 
     let onclick = Callback::from(move |_| {
-        let sorted: Vec<i32> = (1..=50).collect(); // Create a vector with numbers from 1 to 100
-        let data_clone = data.clone();
-        let timeout = Timeout::new(1_000, move || {
-            data_clone.set(sorted);
+        let mut items = (*data).clone();
+        let mut steps = vec![];
+        merge_sort(&mut items, &mut steps);
+        steps.reverse();
+        let items = data.clone();
+        let mut arr = (*data).clone();
+        let interval = Interval::new(time, move || {
+            let item = steps.pop();
+            if item.is_some() {
+                let (index, val) = item.unwrap();
+                arr[index] = val;
+                items.set(arr.clone());
+                web_sys::console::log_1(&JsValue::from(format!("{:?}", item)))
+            }
         });
-        timeout.forget();
+        interval.forget();
+        // let data_clone = data.clone();
+        // let timeout = Timeout::new(1_000, move || {
+        //     data_clone.set(data);
+        // });
+        // timeout.forget();
     });
     html! {
         <>
