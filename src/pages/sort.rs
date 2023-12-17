00009234db67c120ce778bee83f6c813613582f9
Valentin Terebenin <terebenin.work@gmail.com>
2023-12-18 00:27:50 +0300

    fix: fixed shuffle and update buttons

--- src/pages/sort.rs
+++ src/pages/sort.rs
@@ -1,6 +1,7 @@
 use std::collections::VecDeque;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
+use web_sys::console;
 use yew::prelude::*;
 use yew_hooks::use_interval;
 
@@ -11,6 +12,7 @@ use crate::helpers::parse_string_to_i32_or_default;
 use crate::sorting_algorithms::merge_sort::merge_sort;
 
 const MAX_ITEMS: i32 = 500;
+const MS_IN_SECS: i32 = 1000;
 
 pub fn get_output_by_step(items: &mut [i32], steps: VecDeque<(usize, i32)>, step_index: u32) {
     let mut steps = steps.clone();
@@ -61,12 +63,16 @@ pub fn sort() -> Html {
         let steps_time = steps_time.clone();
         let steps_time_value = (*steps_time).clone();
 
+        let interval = interval_ms.clone();
+        let interval_ms = *interval_ms;
+
         let active_step_index = active_step_index.clone();
         use_interval(move || {
             let max = steps.len();
             if *active_step_index as usize >= max {
                 // Clear interval when the end is reached.
-                steps_time.set(0.0);
+                interval.set(0);
+                steps_time.set(0.);
             } else {
                 let step_increment = (max_refresh_rate_ms / steps_time_value).ceil() as usize;
                 let new_step_index = *active_step_index + step_increment as u32;
@@ -75,22 +81,24 @@ pub fn sort() -> Html {
                 } else {
                     new_step_index
                 };
+                console::log_1(&format!("test call").into());
                 get_output_by_step(&mut arr, steps_value.clone(), new_step_index);
                 items.set(arr.clone());
                 active_step_index.set(new_step_index);
             }
-        }, *interval_ms);
+        }, interval_ms);
     }
 
     let handle_sort = {
-        let data = data.clone();
         let time_overall = time_overall.clone();
         let steps_time = steps_time.clone();
         let interval_ms = interval_ms.clone();
         let steps = steps.clone();
+        let items = (*data).clone();
+        let active_step_index = active_step_index.clone();
 
         Callback::from(move |_| {
-            let mut items = (*data).clone();
+            let mut items = items.clone();
             let mut alg_steps = VecDeque::new();
             merge_sort(&mut items, &mut alg_steps);
 
@@ -99,12 +107,9 @@ pub fn sort() -> Html {
             steps.set(alg_steps);
             steps_time.set(time);
             interval_ms.set(time.max(max_refresh_rate_ms) as u32);
+            active_step_index.set(0);
         })
     };
-    {
-        use_effect_with_deps(|v| {
-        }, steps)
-    }
     fn shuffle(mut data: Vec<i32>) -> Vec<i32> {
         let mut rng = thread_rng();
         data.shuffle(&mut rng);
@@ -112,20 +117,28 @@ pub fn sort() -> Html {
     }
     let handle_shuffle = {
         let data = data.clone();
+        let steps = steps.clone();
         Callback::from(move |_| {
             let items = shuffle(data.to_vec());
+            steps.set(VecDeque::new());
             data.set(items);
         })
     };
     let update = {
         let data = data.clone();
         let items_count = items_count.clone();
+        let steps = steps.clone();
         Callback::from(move |_| {
             let items: Vec<i32> = (1..=*items_count).collect();
             let items = shuffle(items);
             data.set(items);
+            steps.set(VecDeque::new());
         })
     };
+    {
+        use_effect_with_deps(|v| {
+        }, steps)
+    }
 
     html! {
             <div class="mx-auto flex justify-center items-center gap-6">
